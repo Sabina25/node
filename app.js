f8f482b8f493e86ea14a9ba110d9ef219c74dbcf
Sabina25 <kadyrova19@gmail.com>
2023-01-22 22:39:51 -0500

    added express and tested it

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require('http');
 const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware.');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the next middleware.');
+    res.send("<h1>Hello from Express!</h1>");
+});
+
+
+const server = http.createServer(app);
 
 server.listen(3000);
