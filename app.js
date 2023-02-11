36119aa74be4faf4f89ab434c109538357521335
Sabina25 <kadyrova19@gmail.com>
2023-02-10 21:39:53 -0500

    work with express

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
+const path = require("path");
+const express = require("express");
 
-const express = require('express');
+const bodyParser = require("body-parser");
+const adminRouters = require("./routes/admin");
+const userRouters = require("./routes/user");
 
 const app = express();
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In the next middleware.');
-    res.send("<h1>Add product page!</h1>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/',(req, res, next) => {
-    console.log('In the next middleware.');
-    res.send("<h1>Hello from Express!</h1>");
+app.use("/admin", adminRouters);
+app.use(userRouters);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html"));
 });
 
 app.listen(3000);
