ec7bebcbfda3fd942f53fbf36dc11ee159bc1809
Sabina25 <kadyrova19@gmail.com>
2023-02-14 13:32:19 -0500

    fix

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const path = require("path");
 const express = require("express");
 
 const bodyParser = require("body-parser");
-const adminRouters = require("./routes/admin");
+const adminData = require("./routes/admin");
 const userRouters = require("./routes/user");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRouters);
+app.use("/admin", adminData.router);
 app.use(userRouters);
 
 app.use((req, res, next) => {
