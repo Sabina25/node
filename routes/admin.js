87b315e29d7b22ba39291425a2aebfdd9b07f238
Sabina25 <kadyrova19@gmail.com>
2023-02-16 14:45:00 -0500

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,23 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
-router.post("/add-product", (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 });
 
-module.exports.router = router;
-module.exports.products = products;
+exports.routes = router;
+exports.products = products;
