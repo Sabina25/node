d186ed6933a3d91517234ec67fd7270d2b756d21
Sabina25 <kadyrova19@gmail.com>
2023-02-18 11:28:31 -0500

    added pages

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -16,15 +16,14 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect("/");
 };
 
-exports.getProducts = (req, res, next) => {
+exports.getAdminProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("admin/product-list", {
+      pageTitle: "Admin Products",
+      path: "/admin/products",
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
       productCSS: true,
+      activeAddProduct: true,
     });
   });
 };
