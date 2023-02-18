9276d3cb7e4bc7b70b93eecc8ef5d42c20f64555
Sabina25 <kadyrova19@gmail.com>
2023-02-17 22:12:15 -0500

    write to file and fetch from file

--- models/product.js
+++ models/product.js
@@ -1,14 +1,40 @@
-const products = [];
+const path = require("path");
+const fs = require("fs");
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, file) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(file);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log("ERR", err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, file) => {
+      if (err) {
+        cb([]);
+      } else {
+        cb(JSON.parse(file));
+      }
+    });
   }
 };
