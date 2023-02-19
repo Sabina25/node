96995160aee332e61db3a22d962dd974f498c40d
Sabina25 <kadyrova19@gmail.com>
2023-02-18 21:05:03 -0500

    added funct

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,6 +19,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  console.log("BODY", req.body);
+  const prodId = req.body.productId;
+  console.log("prodId", prodId.length);
+  res.redirect("/cart");
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
@@ -35,5 +42,27 @@ exports.getCheckout = (req, res, next) => {
     path: "/checkout",
     activeShop: true,
     productCSS: true,
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    pageTitle: "Your Orders",
+    path: "/order",
+    activeShop: true,
+    productCSS: true,
+  });
+};
+
+exports.getProductDetails = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,14 @@ router.get("/", shopController.getIndex);
 
 router.get("/cart", shopController.getCart);
 
+router.post("/cart", shopController.postCart);
+
 router.get("/products", shopController.getProducts);
 
+router.get("/products/:productId", shopController.getProductDetails);
+
 router.get("/checkout", shopController.getCheckout);
+
+router.get("/orders", shopController.getOrders);
 
 module.exports = router;
