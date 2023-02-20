f82166ab96788a2969e413590a019de9d732c92e
Sabina25 <kadyrova19@gmail.com>
2023-02-19 22:15:56 -0500

    added cart-list and delete-cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,27 @@
 const Product = require("../models/product");
 const Cart = require("../models/cart");
 
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
@@ -12,11 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Cart",
-    path: "/cart",
-    activeShop: true,
-    productCSS: true,
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts,
+      });
+    });
   });
 };
 
@@ -28,43 +61,24 @@ exports.postCart = (req, res, next) => {
   res.redirect("/cart");
 };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "Products",
-      path: "/products",
-    });
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    pageTitle: "Checkout",
-    path: "/checkout",
-    activeShop: true,
-    productCSS: true,
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
   });
 };
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
+    path: "/orders",
     pageTitle: "Your Orders",
-    path: "/order",
-    activeShop: true,
-    productCSS: true,
   });
 };
 
-exports.getProductDetails = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
-      activeShop: true,
-      productCSS: true,
-    });
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
   });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const shopController = require("../controllers/shop");
@@ -6,16 +8,18 @@ const router = express.Router();
 
 router.get("/", shopController.getIndex);
 
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct);
+
 router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
 
-router.get("/products", shopController.getProducts);
-
-router.get("/products/:productId", shopController.getProductDetails);
-
-router.get("/checkout", shopController.getCheckout);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
