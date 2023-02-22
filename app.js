0bc12235c7235eb43b9a9620e495a127ba074b53
Sabina25 <kadyrova19@gmail.com>
2023-02-22 13:05:52 -0500

    OTO OTM, cart

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const sequelize = require("./util/database");
 const errorController = require("./controllers/error");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -35,8 +37,12 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
-
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 sequelize
+  //.sync({ force: true })
   .sync()
   .then((result) => {
     return User.findByPk(1);
@@ -48,7 +54,9 @@ sequelize
     return user;
   })
   .then((user) => {
-    console.log("USER", user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
